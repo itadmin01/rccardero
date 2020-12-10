3dd0685ff27925458c47f0a25e8f4890b7fa62c2
IT Admin <admin@itadmin.com.mx>
2020-12-09 19:57:14 -0500

    [ADD] module

---
+++ nomina_cfdi_ee/wizard/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import hr_payroll_payslips_by_employees
+from . import hr_payroll_liquidacion
+from . import import_nomina_xml
---
+++ nomina_cfdi_extras_ee/models/hr_holidays_status.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+from odoo.exceptions import Warning
+
+class HrHolidaysStatus(models.Model):
+    _inherit = 'hr.leave.type'
+    
+   
+    def unlink(self):
+        auto_created_leaves = []
+        if leave_type:
+            auto_created_leaves.append(leave_type.id)
+        for status in self:
+            if status.id in auto_created_leaves:
+                raise Warning("No puedes borrar un registro creado automáticamente %s"%(status.name))
